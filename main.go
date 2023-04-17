f8127c21933d475a46f84910e5138d101692bef3
Tamer Mikhaiel <tamer.mikhaiel@gmail.com>
2023-04-17 20:12:13 +1000

    Testing with csv package

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
+	"fmt"
+	"os"
 )
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
 	flag.Parse()
-	_ = csvFilename
-	// file, err := os.Open(*csvFilename)
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the csv file: %s\n", *csvFilename))
+		os.Exit(1)
+	}
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse the provided csv file.")
+	}
+	fmt.Println(lines)
+}
+func exit(m string) {
+	fmt.Println(m)
+	os.Exit(1)
 }
