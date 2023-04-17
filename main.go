9cafd668bd926d8aec5ae4cd7f9a17e2bfb399b8
Tamer Mikhaiel <tamer.mikhaiel@gmail.com>
2023-04-17 19:56:06 +1000

    Testing with flags package

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"flag"
+)
+
+func main() {
+	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	flag.Parse()
+	_ = csvFilename
+	// file, err := os.Open(*csvFilename)
+}
